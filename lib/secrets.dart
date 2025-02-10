f9acdf3370a433a2660167226eb8f1940a5abbd9
Ram3ez <162774593+Ram3ez@users.noreply.github.com>
2025-02-10 19:00:42 +0530

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,7 +1,9 @@
 class Secrets {
+  static const endpoint = String.fromEnvironment("endpoint"); 
   static const databaseID = String.fromEnvironment("databaseID");
-  static const collectionID = String.fromEnvironment("collectionID");
   static const projectID = String.fromEnvironment("projectID");
   static const documentID = String.fromEnvironment("documentID");
-  static const bucketID = String.fromEnvironment("bucketID");
+  static const collectionID = String.fromEnvironment("collectionID");
+  static const attributeName = String.fromEnvironment("attributeName"); 
+  static const bucketID = String.fromEnvironment("bucketID"); 
 }
